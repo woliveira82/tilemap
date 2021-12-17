485d223952a6e5f6b740b9ffb5ae08ee2bf5165f
woliveira82@gmail.com <woliveira82@gmail.com>
2021-12-16 23:29:32 -0300

    fix: create map with height and width

--- static/js/tile-world.js
+++ static/js/tile-world.js
@@ -6,13 +6,10 @@ var canvas = document.getElementById('main');
 var ctx = canvas.getContext('2d');
 var tileSize = 8;
 var imageNumTiles = 4;
-var rowTileCount = document.getElementById('size').value;
-var colTileCount = document.getElementById('size').value;
 
-function drawImage (ground) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var r = 0; r < rowTileCount; r++) {
-        for (var c = 0; c < colTileCount; c++) {
+function drawImage (ground, width, height) {
+    for (var r = 0; r < width; r++) {
+        for (var c = 0; c < height; c++) {
             var tile = ground[ r ][ c ];
             var tileRow = (tile / imageNumTiles) | 0;
             var tileCol = (tile % imageNumTiles) | 0;
@@ -22,13 +19,19 @@ function drawImage (ground) {
 }
 
 generate_button.addEventListener('click', function(event) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     event.preventDefault();
+    let height = document.getElementById('height').value;
+    let width = document.getElementById('width').value;
+    let rooms = document.getElementById('rooms').value;
+    let halls = document.getElementById('halls').value;
+    let type = document.getElementById('type').value;
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let response = JSON.parse(this.responseText);
-            drawImage(response.map);
+            drawImage(response.map, width, height);
         }
     };
-    xhttp.open('GET', '/maps', true);
+    xhttp.open('GET', `/maps?height=${height}&width=${width}&rooms=${rooms}&halls=${halls}&type=${type}`, true);
     xhttp.send();
 }, false);
